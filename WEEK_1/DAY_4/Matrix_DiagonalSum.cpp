e22b2d9994263fca69504c329328b51a0cf823b0
Abhay-1311 <singhabhaydpr@gmail.com>
2026-06-04 19:57:17 +0530

    Solved Matrix Diagonal Sum

---
+++ WEEK_1/DAY_4/Matrix_DiagonalSum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if((n-i-1)!=i)
+            {
+            sum+=mat[i][n-i-1];
+            sum+=mat[i][i];
+            }
+            else
+            {
+                sum+=mat[i][i];
+            }
+        }
+        return sum;
+    }
+};
